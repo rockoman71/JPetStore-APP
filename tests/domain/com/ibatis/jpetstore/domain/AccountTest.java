49c37259ac831ff3ff08f75f8ed5eed8d7606999
Mike Samano <mss2@urbancode.com>
2018-11-08 08:30:59 -0500

    first commit

---
+++ tests/domain/com/ibatis/jpetstore/domain/AccountTest.java
@@ -0,0 +1,59 @@
+package domain.com.ibatis.jpetstore.domain;
+
+import junit.framework.*;
+
+
+public class AccountTest extends TestCase {
+
+    public AccountTest(String testName) {
+        super(testName);
+    }
+
+    protected void setUp() throws Exception {
+    }
+
+    protected void tearDown() throws Exception {
+    }
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite(AccountTest.class);
+        
+        return suite;
+    }
+
+    /**
+     * Test of matchPassword method, of class xpetstore.domain.Account.
+     */
+    public void testMatchPassword() {
+    }
+
+    /**
+     * Test of getPassword method, of class xpetstore.domain.Account.
+     */
+    public void testGetPassword() {
+    }
+
+    /**
+     * Test of getUserId method, of class xpetstore.domain.Account.
+     */
+    public void testGetUserId() {
+    }
+
+    /**
+     * Test of set method, of class xpetstore.domain.Account.
+     */
+    public void testSet() {
+    }
+
+    /**
+     * Test of setPassword method, of class xpetstore.domain.Account.
+     */
+    public void testSetPassword() {
+    }
+
+    /**
+     * Test of setUserId method, of class xpetstore.domain.Account.
+     */
+    public void testSetUserId() {
+    }
+}
---
+++ tests/domain/com/ibatis/jpetstore/domain/AddressTest.java
@@ -0,0 +1,113 @@
+/*
+ * AddressTest.java
+ * JUnit based test
+ *
+ * Created on September 13, 2006, 11:46 AM
+ */
+
+package domain.com.ibatis.jpetstore.domain;
+
+import junit.framework.*;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Ryan Smith
+ */
+public class AddressTest extends TestCase {
+    
+    public AddressTest(String testName) {
+        super(testName);
+    }
+
+    protected void setUp() throws Exception {
+    }
+
+    protected void tearDown() throws Exception {
+    }
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite(AddressTest.class);
+        
+        return suite;
+    }
+
+    /**
+     * Test of getCity method, of class xpetstore.domain.Address.
+     */
+    public void testGetCity() {
+    }
+
+    /**
+     * Test of getCountry method, of class xpetstore.domain.Address.
+     */
+    public void testGetCountry() {
+    }
+
+    /**
+     * Test of getState method, of class xpetstore.domain.Address.
+     */
+    public void testGetState() {
+    }
+
+    /**
+     * Test of getStreet1 method, of class xpetstore.domain.Address.
+     */
+    public void testGetStreet1() {
+    }
+
+    /**
+     * Test of getStreet2 method, of class xpetstore.domain.Address.
+     */
+    public void testGetStreet2() {
+    }
+
+    /**
+     * Test of getZipcode method, of class xpetstore.domain.Address.
+     */
+    public void testGetZipcode() {
+    }
+
+    /**
+     * Test of set method, of class xpetstore.domain.Address.
+     */
+    public void testSet() {
+    }
+
+    /**
+     * Test of setCity method, of class xpetstore.domain.Address.
+     */
+    public void testSetCity() {
+    }
+
+    /**
+     * Test of setCountry method, of class xpetstore.domain.Address.
+     */
+    public void testSetCountry() {
+    }
+
+    /**
+     * Test of setState method, of class xpetstore.domain.Address.
+     */
+    public void testSetState() {
+    }
+
+    /**
+     * Test of setStreet1 method, of class xpetstore.domain.Address.
+     */
+    public void testSetStreet1() {
+    }
+
+    /**
+     * Test of setStreet2 method, of class xpetstore.domain.Address.
+     */
+    public void testSetStreet2() {
+    }
+
+    /**
+     * Test of setZipcode method, of class xpetstore.domain.Address.
+     */
+    public void testSetZipcode() {
+    }
+    
+}
